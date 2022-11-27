f2ca4579585165796279cdce4e79614a18b7070b
chandan0c <chandan0c098@gmail.com>
2022-11-27 20:30:17 +0530

    deploy fix

--- src/task/Contact.js
+++ src/task/Contact.js
@@ -5,18 +5,18 @@ function Contact() {
     <div  >
        <h1  >Contact Me</h1>
        <h3 >
-         <a  href='https://www.linkedin.com/in/chandan-giri-9746a6238/' target="_blank">Linkdin</a>
+         <a  href='https://www.linkedin.com/in/chandan-giri-9746a6238/' target="_blank" rel="linkd">Linkdin</a>
        </h3>
        <h3 >
-         <a href='https://github.com/' target="_blank">Git Hub</a>
+         <a href='https://github.com/' target="_blank" rel="gitHus">Git Hub</a>
        </h3>
        <h3 >
          {/*<span >chandan0c098@gmail.com</span>*/}
-         <a href='https://mail.google.com/mail/u/1/?view=cm&fs=1&to=chandan0c@gmail.com&tf=1' target="_blank">Gmail</a>
+         <a href='https://mail.google.com/mail/u/1/?view=cm&fs=1&to=chandan0c@gmail.com&tf=1' target="_blank" rel="gma">Gmail</a>
        </h3>
        <h3 >
          {/*<span >+91 7441129480</span>*/}
-         <a href="tel:7441129480" target="_blank">Contact No :7441129480</a>
+         <a href="tel:7441129480" target="_blank" rel="contactr">Contact No :7441129480</a>
        </h3>
        <h2>Designed & Built by <span>Chandan</span> © 2022</h2>
     </div>
